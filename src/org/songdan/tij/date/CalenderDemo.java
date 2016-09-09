6855ba47caceec809ff7b48fbdcdf6b302446c96
sd1990 <songdan_obj@126.com>
2016-09-09 17:11:32 +0800

    测试Calendar的获取时间问题

---
+++ src/org/songdan/tij/date/CalenderDemo.java
@@ -0,0 +1,28 @@
+package org.songdan.tij.date;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Songdan
+ * @date 2016/9/9 16:25
+ */
+public class CalenderDemo {
+    static Calendar instance = Calendar.getInstance();
+
+    static DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public static void main(String[] args) throws InterruptedException {
+        Date time = instance.getTime();
+        System.out.println(df.format(time));
+        System.out.println(System.currentTimeMillis());
+        TimeUnit.SECONDS.sleep(30);
+        Date time1 = instance.getTime();
+        System.out.println(df.format(time1));
+        System.out.println(System.currentTimeMillis());
+    }
+
+}
