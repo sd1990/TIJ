74b27afc94a27135f91c4dd5452f263dd47a60e8
sd1990 <songdan_obj@126.com>
2016-10-25 10:36:36 +0800

    使用不同的类加载器加载相同的class，类的类型是不相同的。

--- src/org/songdan/tij/classloader/RepeatLoadDemo.java
+++ src/org/songdan/tij/classloader/RepeatLoadDemo.java
@@ -4,15 +4,25 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
+ * 使用不同的类加载器加载相同的class，类的类型是不相同的。
+ * 在类加载的过程中，会加载相关类的父类或接口
  * @author Songdan
  * @date 2016/7/6
  */
 public class RepeatLoadDemo {
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         ClassLoaderCase classLoaderCase = new ClassLoaderCase();
-        classLoaderCase.loadClass("org.songdan.tij.classloader.RepeatLoadDemo");
-        classLoaderCase.loadClass("org.songdan.tij.classloader.RepeatLoadDemo");
+        Class<?> aClass = null;
+        try {
+            aClass = classLoaderCase.loadClass("org.songdan.tij.classloader.RepeatLoadDemo");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Object instance = aClass.newInstance();
+        System.out.println("===================");
+        System.out.println(instance.getClass());
+        System.out.println(instance instanceof RepeatLoadDemo );
     }
 
 }
@@ -21,8 +31,9 @@ class ClassLoaderCase extends ClassLoader{
 
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-        String fileName = name.substring(0, name.lastIndexOf(".")) + ".class";
+        String fileName = name.substring(name.lastIndexOf(".")+1) + ".class";
         System.out.println(fileName);
+        System.out.println("=======================");
         InputStream in = getClass().getResourceAsStream(fileName);
         if (in==null) {
             return super.loadClass(name);
