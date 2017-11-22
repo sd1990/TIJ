f5a9c78c6b98e6c0d50c80a3980766b5ddbc5bf7
dan.song <dan.song@wormpex.com>
2017-11-23 00:51:18 +0800

    优化接竹竿算法

--- algorithm/src/main/java/org/songdan/tij/algorithm/queue/PokerGame.java
+++ algorithm/src/main/java/org/songdan/tij/algorithm/queue/PokerGame.java
@@ -1,11 +1,12 @@
 package org.songdan.tij.algorithm.queue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 
 /**
@@ -25,31 +26,56 @@ public class PokerGame {
 		this.desktop = desktop;
 	}
 
+	public static void main(String[] args) {
+		Gamer one = new Gamer("张三");
+		Gamer two = new Gamer("李四");
+		Gamer three = new Gamer("王五");
+		ArrayList<Gamer> gamers = new ArrayList<>();
+		gamers.add(one);
+		gamers.add(two);
+		gamers.add(three);
+		new PokerGame(gamers, new Desktop()).play();
+		System.out.println("<<<<<<<<<<<<<>>>>>>>>>>>>>");
+		new PokerGame(gamers, new NewDesktop()).play();
+	}
+
 	public void play() {
-		init();
+		long start = System.currentTimeMillis();
+//		init();
+		init2();
+		int sum = 0;
 		int index = 0;
-		Gamer gamer = gamers.get(index);
+		Gamer gamer = gamers.get(0);
 		while (!isOver()) {
 			List<Integer> cards = desktop.receiveCard(gamer.outCard());
 			if (cards.size() == 0) {
 				//下一个人出牌
-				gamer = gamers.get(++index%gamers.size());
-			}else{
+				gamer = gamers.get(++index % gamers.size());
+				while (gamer.isOver()) {
+					gamer = gamers.get(++index % gamers.size());
+				}
+			}
+			else {
 				gamer.addCard(cards);
 				//还是这个人出牌 不变
 			}
-			desktop.show();
+			sum++;
+//			desktop.show();
 		}
-		System.out.println("winner is:"+gamer);
+		System.out.println("winner is:" + gamer + ", sum is "+sum+" ,time cost is :" + (System.currentTimeMillis() - start));
 	}
 
-	public static void main(String[] args) {
-		Gamer one = new Gamer("张三");
-		Gamer two = new Gamer("李四");
-		ArrayList<Gamer> gamers = new ArrayList<>();
-		gamers.add(one);
-		gamers.add(two);
-		new PokerGame(gamers,new Desktop()).play();
+	private void init2() {
+		for (Gamer gamer : gamers) {
+			gamer.reset();
+		}
+		String cards = "12, 8, 10, 6, 8, 12, 12, 14, 5, 4, 12, 13, 8, 9, 10, 4, 2, 6, 1, 3";
+		String cards2 = "13, 15, 6, 1, 11, 13, 15, 5, 3, 9, 7, 7, 14, 10, 11, 2, 2, 15, 7, 3";
+		String cards3 = "6, 11, 9, 5, 1, 11, 4, 14, 7, 3, 15, 13, 8, 4, 10, 5, 2, 14, 1, 9";
+
+		gamers.get(0).addCard(Arrays.stream(cards.split(",")).map(str -> Integer.parseInt(str.trim())).collect(Collectors.toList()));
+		gamers.get(1).addCard(Arrays.stream(cards2.split(",")).map(str -> Integer.parseInt(str.trim())).collect(Collectors.toList()));
+		gamers.get(2).addCard(Arrays.stream(cards3.split(",")).map(str -> Integer.parseInt(str.trim())).collect(Collectors.toList()));
 	}
 
 	private boolean isOver() {
@@ -58,17 +84,24 @@ public class PokerGame {
 		}).count() == 1;
 	}
 
-	private void init(){
-		//初始化游戏
-		gamers.forEach(gamer ->{
-			ThreadLocalRandom localRandom = ThreadLocalRandom.current();
-			List<Integer> cards = new ArrayList<>();
-			for (int i = 0; i < 10; i++) {
-				int card = localRandom.nextInt(1, 15);
-				cards.add(card);
+	private void init() {
+		for (Gamer gamer : gamers) {
+			gamer.reset();
+		}
+		List<Integer> allCards = new ArrayList<>();
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 15; j++) {
+				allCards.add(j + 1);
 			}
-			gamer.addCard(cards);
-		});
+		}
+		Collections.shuffle(allCards);
+		int index = 0;
+		for (Integer card : allCards) {
+			gamers.get(index++ % gamers.size()).add(card);
+		}
+		for (Gamer gamer : gamers) {
+			gamer.show();
+		}
 	}
 
 }
@@ -76,7 +109,7 @@ public class PokerGame {
 /**
  * 参与游戏的人
  */
-class Gamer{
+class Gamer {
 
 	private String name;
 
@@ -88,17 +121,31 @@ class Gamer{
 
 	public Integer outCard() {
 		Integer card = holder.removeFirst();
-		System.out.println("gamer :"+name+"->"+card);
+//		System.out.println("gamer :" + name + "->" + card);
 		return card;
 	}
 
 	public void addCard(List<Integer> cards) {
-		System.out.println("gamer :"+name+"<-"+cards);
+//		System.out.println("gamer :" + name + "<-" + cards);
 		holder.addAll(cards);
 	}
 
-	public boolean isOver(){
+	public boolean isOver() {
 		return holder.isEmpty();
+	}
+
+	public void add(Integer card) {
+
+		holder.addLast(card);
+
+	}
+
+	public void reset() {
+		holder.clear();
+	}
+
+	public void show() {
+		System.out.println(name + " : " + holder);
 	}
 
 	@Override
@@ -110,10 +157,10 @@ class Gamer{
 /**
  * 牌桌
  */
-class Desktop{
+class Desktop {
 
 
-	private LinkedList<Integer> holder = new LinkedList<>();
+	protected LinkedList<Integer> holder = new LinkedList<>();
 
 
 	/**
@@ -144,4 +191,36 @@ class Desktop{
 		System.out.println(holder);
 	}
 
+}
+
+class NewDesktop extends Desktop {
+
+	private int[] book = new int[16];
+
+	@Override
+	public List<Integer> receiveCard(Integer card) {
+		if (book[card] == 0) {
+			holder.addLast(card);
+			book[card] = 1;
+			return Collections.emptyList();
+		}
+		else {
+			List<Integer> container = new ArrayList<>();
+			container.add(card);
+			Iterator<Integer> reverseIterator = holder.descendingIterator();
+			while (reverseIterator.hasNext()) {
+				Integer next = reverseIterator.next();
+				container.add(next);
+				//标记清空
+				book[next] = 0;
+				//移除元素
+				reverseIterator.remove();
+				if (next.equals(card)) {
+					//清除container
+					return container;
+				}
+			}
+		}
+		return Collections.emptyList();
+	}
 }
