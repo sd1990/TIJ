62868d8696f2875e113fe309b4f98edc16075746
sd1990 <songdan_obj@126.com>
2020-11-03 16:21:08 +0800

    will-revert:提交shuju

--- base/src/main/java/org/songdan/tij/string/StringDemo.java
+++ base/src/main/java/org/songdan/tij/string/StringDemo.java
@@ -90,3 +90,6 @@ public class StringDemo {
         System.out.println(new MessageFormat("{0,number,#0.00}--{1,number,#0.00}--{2,number,#0.00}--{3,number,#0.00}").format(new Object[]{1,2.1,2.11,0.1}));
     }
 }
+
+
+要撤销的内容
